1b068118ad0bfc1e149616a785cf9819b3b78a1d
AlonsoErnesto <ernesto134alonso123@gmail.com>
2023-07-30 20:40:58 -0500

    Education v1.0

--- components/Education/index.tsx
+++ components/Education/index.tsx
@@ -8,16 +8,52 @@ export const Education = () => {
          </div>
          <div className={styles.content}>
             <div className={styles.education}>
-                  Educacion
+               <div className={styles.info}>
+                  <h1>Ingenieria de Sistemas</h1>
+                  <p>
+                     Universidad Peruana de Ciencias Aplicadas (UPC)<br/>
+                     <span style={{fontSize:18}}>Estudiante</span>
+                     </p>
+               </div>
+               <div className={styles.logo}>
+                  <img src="./assets/images/education/upc.png"/>
+               </div>
             </div>
             <div className={styles.education}>
-               Educacion
+               <div className={styles.info}>
+                  <h1>Desarrollo WEB Full Stack</h1>
+                  <p>
+                     Intituto de Educacion Superior TECSUP <br/>
+                     <span style={{fontSize:18}}>2021</span>
+                  </p>
+               </div>
+               <div className={styles.logo}>
+                  <img src="./assets/images/education/tecsup.png"/>
+               </div>
             </div>
             <div className={styles.education}>
-               Educacion
+               <div className={styles.info}>
+                  <h1>Desarrollador de Sistemas</h1>
+                  <p>
+                     Instituto tecnologico de Arequipa UNITEK<br/>
+                     <span style={{fontSize:18}}>2021</span>
+                  </p>
+               </div>
+               <div className={styles.logo}>
+                  <img src="./assets/images/education/unitek.png"/>
+               </div>
             </div>
             <div className={styles.education}>
-               Educacion
+               <div className={styles.info}>
+                  <h1>ReactJS / Express / MongoDB</h1>
+                  <p>
+                     Juan Pablo de la Torre - Fernando Herrera<br/>
+                     <span style={{fontSize:18}}>2022</span>
+                  </p>
+               </div>
+               <div className={styles.logo}>
+                  <img src="./assets/images/education/udemy.png"/>
+               </div>
             </div>
          </div>
       </div>
